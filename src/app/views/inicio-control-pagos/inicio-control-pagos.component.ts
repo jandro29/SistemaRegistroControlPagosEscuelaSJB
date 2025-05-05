25313f28c50861db8890a78defd282a16c5b656c
jandro29 <ascolanr@gmail.com>
2025-05-05 02:33:10 -0500

    segundo commit

---
+++ src/app/views/inicio-control-pagos/inicio-control-pagos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-control-pagos',
+  imports: [],
+  templateUrl: './inicio-control-pagos.component.html',
+  styleUrl: './inicio-control-pagos.component.css'
+})
+export class InicioControlPagosComponent {
+
+}
