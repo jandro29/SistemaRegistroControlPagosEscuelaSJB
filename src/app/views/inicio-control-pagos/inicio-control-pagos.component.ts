3299fe39f5129f61b9ac9fb208e4d475f9cb6169
jandro29 <ascolanr@gmail.com>
2025-05-05 23:46:45 -0500

    tercer commit

--- src/app/views/inicio-control-pagos/inicio-control-pagos.component.ts
+++ src/app/views/inicio-control-pagos/inicio-control-pagos.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MenuDesplegableComponent } from '../menu-desplegable/menu-desplegable.component';
 
 @Component({
   selector: 'app-inicio-control-pagos',
-  imports: [],
+  imports: [MenuDesplegableComponent],
   templateUrl: './inicio-control-pagos.component.html',
   styleUrl: './inicio-control-pagos.component.css'
 })
