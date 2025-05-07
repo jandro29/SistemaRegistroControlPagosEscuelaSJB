9bbd8cbaef7a71e7ab0f481a5eca6f83295423ef
jandro29 <ascolanr@gmail.com>
2025-05-07 01:48:09 -0500

    .

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -16,8 +16,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 28564, hash: 'eac97c19fc9e3b9ce5359e630b9f5a5ccbf4677548cb9ac7e41e55b2bfcaea2d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17152, hash: 'c23928128a2ba08c4d5922b5f9e3452d1ae9924ac915672cbc1a9887602a1b2b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 28564, hash: '4da4c9988059951e8074bfe17c2d72fbdba6255fb015d60c7842e4d9018ec8c8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17152, hash: '34add85b71b188f51c319da4380086b4e07487a9bda15a1828f108789e8f3a2f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'inicio-control-de-pagos/index.html': {size: 33801, hash: '71b22c086f44b95e72f9afb59f092cdb767a0db06f18c69b19a77172f6fcd696', text: () => import('./assets-chunks/inicio-control-de-pagos_index_html.mjs').then(m => m.default)},
     'index.html': {size: 87755, hash: 'f8dcf096411d272142aa22f04b91b6bfb88a098fb9d09380605a0402ee1e4683', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5YCQL7LF.css': {size: 20263, hash: 'Ol7ROH0dcPE', text: () => import('./assets-chunks/styles-5YCQL7LF_css.mjs').then(m => m.default)}
